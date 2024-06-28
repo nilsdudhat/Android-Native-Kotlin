7bef403a30f0d3a5cd11d70c80c7a68312b9eb18
nilsdudhat <nilsdudhat14@gmail.com>
2024-06-28 07:00:29 +0530

    minor

---
+++ Udemy-Tutorials/Drawer-Android-Kotlin/app/src/main/java/com/udemy/drawer/app/ui/gallery/GalleryFragment.kt
@@ -0,0 +1,40 @@
+package com.udemy.drawer.app.ui.gallery
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.udemy.drawer.app.databinding.FragmentGalleryBinding
+
+class GalleryFragment : Fragment() {
+
+    private var _binding: FragmentGalleryBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val galleryViewModel =
+            ViewModelProvider(this)[GalleryViewModel::class.java]
+
+        _binding = FragmentGalleryBinding.inflate(inflater, container, false)
+
+        galleryViewModel.text.observe(viewLifecycleOwner) {
+            binding.textGallery.text = it
+        }
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
